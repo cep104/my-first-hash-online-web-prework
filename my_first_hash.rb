633d9cc40bf4f05b88f09c5e9ff166b1cc9caf0f
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-25 16:10:17 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,7 +2,7 @@ def my_hash
  your_hash = {"name" = "Candice"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-  
+ end 
 end
 
 
